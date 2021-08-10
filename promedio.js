2dbb0638d64eecd8bee3908b96d5dff3584bde02
Fred-Cach <federicocachero@hotmail.com>
2021-08-10 02:36:48 -0300

    added index.html

--- promedio.js
+++ promedio.js
@@ -4,7 +4,7 @@ function calcularMediaAritmetica(lista) {
   /* let sumaLista = 0;
     for (let i = 0; i < lista.length; i++) {
     sumaLista = sumaLista + lista[i];
-    }; */
+    };   */
 
   const sumaLista = lista.reduce((valorAcumulado = 0, nuevoElemento) => {
     return valorAcumulado + nuevoElemento;
